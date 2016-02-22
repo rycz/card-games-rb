e52103d9ab0ee0769a397a925eae0a769e211354
Jennifer Gryczkowski <accounts@rycz.com>
2016-02-21 19:51:10 -0600

    Update war.rb
    Added the new Player class as well as a hash of Stacks, consolidating different variables so they can be called with one of two symbol keys. Combined methods to remove repetition.

--- war.rb
+++ war.rb
@@ -8,46 +8,35 @@ helpers do
     def set_up_game
         session[:deck] = Deck.new
         session[:deck].shuffle
-        session[:player1] = Stack.new
-        session[:player2] = Stack.new
+        session[:player1] = Player.new "Player 1"
+        session[:player2] = Player.new "Player 2"
         deal_cards
     end
     
     def deal_cards
+        session[:player2].take session[:deck].draw
         while session[:deck].not_empty
             session[:player1].take session[:deck].draw
-            session[:player2].take session[:deck].draw
+            
         end
     end
     
     def new_battle
-        @p1_played = Stack.new
-        @p2_played = Stack.new
+        @cards_in_play = {}
+        @cards_in_play[:player1] = Stack.new
+        @cards_in_play[:player2] = Stack.new
         @results = ""
         @game_over = false
     end
     
-    def player1_plays_card num=1
+    def take_turn_playing_cards(player, num=1)
         while num > 0
-            if player1_loses
+            if the_loser_is player
                 @game_over = true
-                @results += "Player 1 loses!<br>GAME OVER<br>"
+                print_losing_message_for player
                 num = 0
             else
-                @p1_played.take session[:player1].play_card
-                num -= 1
-            end
-        end
-    end
-    
-    def player2_plays_card num=1
-        while num > 0
-            if player2_loses
-                @game_over = true
-                @results += "Player 2 loses!<br>GAME OVER<br>"
-                num = 0
-            else
-                @p2_played.take session[:player2].play_card
+                @cards_in_play[player].take session[player].play_card
                 num -= 1
             end
         end
@@ -55,17 +44,17 @@ helpers do
     
     def do_battle
         new_battle
-        player1_plays_card
-        player2_plays_card
+        take_turn_playing_cards(:player1)
+        take_turn_playing_cards(:player2)
         choose_the_winner
     end
 
     def choose_the_winner
         if !@game_over
-            if (@p1_played.current_card.value > @p2_played.current_card.value)
-                player1_wins
-            elsif (@p2_played.current_card.value > @p1_played.current_card.value)
-                player2_wins
+            if (@cards_in_play[:player1].current_card.value > @cards_in_play[:player2].current_card.value)
+                the_winner_is :player1
+            elsif (@cards_in_play[:player2].current_card.value > @cards_in_play[:player1].current_card.value)
+                the_winner_is :player2
             else
                 war
             end
@@ -75,73 +64,68 @@ helpers do
     # if the players tie, they go to war
     def war
         declare_war
-        # each player puts one card face down and another card face up
-        player1_plays_card 2
-        player2_plays_card 2
+        # each player puts one card face down and another card face up, playing 2 cards total
+        take_turn_playing_cards(:player1, 2)
+        take_turn_playing_cards(:player2, 2)
         choose_the_winner
     end
     
     def declare_war
-        @results += "Player 1 plays #{ @p1_played.current_card.description } and Player 2 plays #{ @p2_played.current_card.description }.<br>THIS MEANS WAR!<br>"
+        @results += "#{ session[:player1].name } plays #{ @cards_in_play[:player1].current_card.description } and #{ session[:player2].name } plays #{ @cards_in_play[:player2].current_card.description }.<br>THIS MEANS WAR!<br>"
     end
     
-    def player1_wins
-        @results += "Player 1 wins with #{ @p1_played.current_card.description } versus #{ @p2_played.current_card.description }.<br>"
-        collect_pot
-        while @pot.not_empty
-            session[:player1].take @pot.draw
-            @results += "Player 1 gains #{ session[:player1].current_card.description }.<br>"
+    def the_winner_is winner
+        if winner == :player1
+            loser = :player2
+        else
+            loser = :player1
         end
-        output_number_of_cards
-    end
-    
-    def player2_wins
-        @results += "Player 2 wins with #{ @p2_played.current_card.description } versus #{ @p1_played.current_card.description }.<br>"
+        @results += "#{ session[winner].name } wins with #{ @cards_in_play[winner].current_card.description } versus #{ @cards_in_play[loser].current_card.description }.<br>"
         collect_pot
         while @pot.not_empty
-            session[:player2].take @pot.draw
-            @results += "Player 2 gains #{ session[:player2].current_card.description }.<br>"
+            session[winner].take @pot.draw
+            @results += "#{ session[winner].name } gains #{ session[winner].current_card.description }.<br>"
         end
         output_number_of_cards
     end
 
     def output_number_of_cards
-        @results += "Player 1 has #{ session[:player1].deck_size } cards.<br>Player 2 has #{ session[:player2].deck_size } cards.<br>"
+        @results += "#{ session[:player1].name } has #{ session[:player1].num_of_cards_left } cards.<br>#{ session[:player2].name } has #{ session[:player2].num_of_cards_left } cards.<br>"
     end
     
-    def player1_loses
-        !session[:player1].not_empty
-    end
-    
-    def player2_loses
-        !session[:player2].not_empty
+    def the_loser_is player
+        session[player].loses
     end
 
     # combines the cards played by players 1 and 2 into a pot and shuffles them
     def collect_pot
         @pot = Stack.new
-        while @p1_played.not_empty
-            @pot.take @p1_played.draw
-        end
-        while @p2_played.not_empty
-            @pot.take @p2_played.draw
-        end
+        grab_cards_from :player1
+        grab_cards_from :player2
         @pot.shuffle
+    end
+    
+    def grab_cards_from player
+        while @cards_in_play[player].not_empty
+            @pot.take @cards_in_play[player].draw
+        end
+    end
+    
+    def print_losing_message_for player
+        @results += "#{ session[player].name } loses!<br>GAME OVER<br><a href='/'>Play Again</a><br>"
     end
 
     # if neither player has lost, it asks to continue
     # if one of the players has lost, it asks to play again
     def ask_to_continue
         if !@game_over
-            if player1_loses
-                @results += "Player 1 loses!<br>GAME OVER<br><a href='/'>Play Again</a><br>"
-            elsif player2_loses
-                @results += "Player 2 loses!<br>GAME OVER<br><a href='/'>Play Again</a><br>"
+            if the_loser_is :player1
+                print_losing_message_for :player1
+            elsif the_loser_is :player2
+                print_losing_message_for :player2
             else
                 @results += "<a href='/play/battle'>Click to play next turn.</a>"
             end
-        else
-            @results += "<a href='/'>Play Again</a>"
         end
     end
 
